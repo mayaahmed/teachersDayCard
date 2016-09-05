ca359dd4ce0bfc072d9c0c1b4081bd4aa68387e2
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2016-09-05 21:25:33 +0530

    Add files via upload

---
+++ card.js
@@ -0,0 +1,22 @@
+
+card1 = document.getElementById('card1'); 
+card2 = document.getElementById('card2'); 
+card3 = document.getElementById('card3');
+card2.style.visibility="hidden";
+card3.style.visibility="hidden";
+
+function run(i){
+  if(i==1) slideOpen(card1,card2);  
+  if(i==2) slideOpen(card2,card3);
+}
+
+
+
+function slideOpen(el,card){
+el.style.transition="width 4s linear 0s";
+el.style.width="0.5%";
+card.style.visibility="visible";
+}
+
+
+run();
